dc26719fb6064a18b605dfa1773e8456762896a6
stringly <magnificentstringly@gmail.com>
2018-09-10 06:49:53 -0400

    Add working demoe to Home view

--- OrgChartDemo/Startup.cs
+++ OrgChartDemo/Startup.cs
@@ -13,6 +13,7 @@ namespace OrgChartDemo {
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,14 +22,12 @@ namespace OrgChartDemo {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
